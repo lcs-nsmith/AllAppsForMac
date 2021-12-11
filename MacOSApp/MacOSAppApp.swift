df53358bb0929eaa742e14adb68cb316a123c42a
Nathan Smith <nsmith@lcs.on.ca>
2021-12-11 09:48:43 -0500

    Initial Commit

---
+++ MacOSApp/MacOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSAppApp.swift
+//  MacOSApp
+//
+//  Created by Nate S on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct MacOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
